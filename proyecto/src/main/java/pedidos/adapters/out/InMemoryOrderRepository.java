e53600e299690c24d7c47426012214a4c7f24b0c
garrett <gsdz3242@gmail.com>
2026-05-18 22:35:54 -0500

    clase InMemoryOrderRepository finalizada

--- proyecto/src/main/java/pedidos/adapters/out/InMemoryOrderRepository.java
+++ proyecto/src/main/java/pedidos/adapters/out/InMemoryOrderRepository.java
@@ -1,7 +1,38 @@
 package pedidos.adapters.out;
 
+import pedidos.domain.Order;
 import pedidos.ports.out.OrderRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InMemoryOrderRepository implements OrderRepository {
 
+    // lista donde se guardarán los pedidos
+    private List<Order> orders = new ArrayList<>();
+
+
+    @Override
+    public void save(Order order) {
+
+        if (order == null) {
+            throw new IllegalArgumentException("el pedido no puede ser null");
+        }
+
+        orders.add(order);
+    }
+
+
+    @Override
+    public Order findOrderById(int id) {
+
+        for (Order order : orders) {
+
+            if (order.getId() == id) {
+                return order;
+            }
+        }
+
+        return null;
+    }
 }
