fb7e89e50e968823dba439da4f1764bdf108d953
garrett <gsdz3242@gmail.com>
2026-05-21 23:08:08 -0500

    capa aplication finalizada (CreateOrderUseCase, GetOrderUseCase)

--- proyecto/src/main/java/pedidos/application/CreateOrderUseCase.java
+++ proyecto/src/main/java/pedidos/application/CreateOrderUseCase.java
@@ -1,4 +1,24 @@
 package pedidos.application;
 
+import pedidos.domain.Order;
+import pedidos.ports.out.OrderRepository;
+
 public class CreateOrderUseCase {
+    private OrderRepository repository;
+
+    // inyección de dependencia
+    public CreateOrderUseCase(OrderRepository repository) {
+        this.repository = repository;
+    }
+
+    public void createOrder(Order order) {
+
+        if (order == null) {
+            throw new IllegalArgumentException("el pedido no puede ser null");
+        }
+
+        repository.save(order);
+    }
+
+
 }
--- proyecto/src/main/java/pedidos/application/GetOrderUseCase.java
+++ proyecto/src/main/java/pedidos/application/GetOrderUseCase.java
@@ -1,4 +1,22 @@
 package pedidos.application;
+import pedidos.domain.Order;
+import pedidos.ports.out.OrderRepository;
 
 public class GetOrderUseCase {
+
+    private OrderRepository repository;
+
+    // inyección de dependencia
+    public GetOrderUseCase(OrderRepository repository) {
+        this.repository = repository;
+    }
+
+    public Order findById(int id) {
+        if (id < 0){
+            throw new IllegalArgumentException("el id no puede ser negativo");
+        }
+
+        return repository.findOrderById(id);
+    }
+
 }
