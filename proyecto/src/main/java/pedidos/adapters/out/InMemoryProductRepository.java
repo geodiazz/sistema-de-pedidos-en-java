eccedfa2cc650b3500962769f051a9c850195b28
garrett <gsdz3242@gmail.com>
2026-06-15 22:03:03 -0500

    se corrigio un error en el nombre de una funcion en ProductRepository, se creo toda la estructura de la clase InMemoryProductRepository nesesitando correcciones.

--- proyecto/src/main/java/pedidos/adapters/out/InMemoryProductRepository.java
+++ proyecto/src/main/java/pedidos/adapters/out/InMemoryProductRepository.java
@@ -1,11 +1,30 @@
 package pedidos.adapters.out;
-
 import pedidos.domain.Product;
 import pedidos.ports.out.ProductRepository;
 
+import java.util.List;
 import java.util.Map;
 
 public class InMemoryProductRepository implements ProductRepository {
 
     private Map<Integer, Product> products;
+
+    public void save(Product product) {
+
+       //tengo una duda con la siguiente linea de codigo, la forma de asignar el integer es correcta?
+        products.put(product.getIdProducto(), product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        if (products.containsKey(id)){
+            return products.get(id);
+        }
+       return null;
+    }
+    //el mismo ide me sugirio la siguiente funcion tal cual esta, la verdad no conozco la forma de hacerlo por mi cuenta
+    @Override
+    public List<Product> findAll() {
+        return List.of();
+    }
 }
--- proyecto/src/main/java/pedidos/ports/out/ProductRepository.java
+++ proyecto/src/main/java/pedidos/ports/out/ProductRepository.java
@@ -2,11 +2,10 @@ package pedidos.ports.out;
 
 import pedidos.domain.Product;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface ProductRepository {
     void save(Product product);
-    Product findProductById(int id);
+    Product findById(int id);
      List<Product> findAll();
 }
