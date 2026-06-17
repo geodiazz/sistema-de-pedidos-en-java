034e2135af2aa32160a7591ef8a53c86f04919f8
garrett <gsdz3242@gmail.com>
2026-06-16 22:00:19 -0500

    se finalizo la clase InMemoryProductRepository

--- proyecto/src/main/java/pedidos/adapters/out/InMemoryProductRepository.java
+++ proyecto/src/main/java/pedidos/adapters/out/InMemoryProductRepository.java
@@ -2,29 +2,25 @@ package pedidos.adapters.out;
 import pedidos.domain.Product;
 import pedidos.ports.out.ProductRepository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class InMemoryProductRepository implements ProductRepository {
 
-    private Map<Integer, Product> products;
+    private Map<Integer, Product> products = new HashMap<>();
 
     public void save(Product product) {
-
-       //tengo una duda con la siguiente linea de codigo, la forma de asignar el integer es correcta?
         products.put(product.getIdProducto(), product);
     }
 
     @Override
     public Product findById(int id) {
-        if (products.containsKey(id)){
-            return products.get(id);
-        }
-       return null;
+       return products.get(id);
     }
-    //el mismo ide me sugirio la siguiente funcion tal cual esta, la verdad no conozco la forma de hacerlo por mi cuenta
     @Override
     public List<Product> findAll() {
-        return List.of();
+        return new ArrayList<>(products.values());
     }
 }
